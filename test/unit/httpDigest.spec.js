edce591aa06946f2737126d2e5ccd413f7210377
Dmitri Zagidulin <dzagidulin@gmail.com>
2019-12-02 19:07:30 -0500

    Initial commit.

---
+++ test/unit/httpDigest.spec.js
@@ -0,0 +1,23 @@
+/*!
+ * Copyright (c) 2019 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+import httpDigest from '../../lib';
+import chai from 'chai';
+
+chai.should();
+const { expect } = chai;
+
+describe('http-signature-digest', () => {
+  describe('createDigestString', () => {
+    it('should create a digest of a given string', async () => {
+      const data = `{"hello": "world"}`;
+      const result = await httpDigest.create(
+        {data, algorithm: 'sha256', useMultihash: false}
+      );
+      result.should
+        .equal('SHA-256=X48E9qOokqqrvdts8nOJRJN3OWDUoyWxBf7kbu9DBPE=');
+    });
+  });
+});
